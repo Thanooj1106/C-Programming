1433f048f9bb104c2e8f794e2ca578154f12e23c
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-08 13:55:43 +0530

    Add files via upload

---
+++ Fahrenheit to celsius scale.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	float fahrenheit,celsius;
+	printf("Enter temperature in fahrenheit:");
+	scanf("%f",&fahrenheit);
+	celsius = (fahrenheit-32)*5/9;
+	printf("Temperature in celsius is:%f",celsius);
+	return 0;
+}
