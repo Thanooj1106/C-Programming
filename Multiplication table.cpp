19a4127a1dbbff89e9c06bc126143c119d6b9736
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-12 14:10:13 +0530

    Add files via upload

--- Multiplication table.cpp
+++ Multiplication table.cpp
@@ -1,14 +1,10 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int main()
 {
-    int n, i;
-
-    printf("Enter a number:");
-    scanf("%d", &n);
-
-    for (i = 1; i <= 10; i++)
-        printf("%d x %d = %d\n", n, i, n * i);
-
-    return 0;
+	int n,i;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	for (i=1;i<=10;i++)
+	printf("%d x %d =%d\n",n,i,n*i);
+	return 0;
 }
