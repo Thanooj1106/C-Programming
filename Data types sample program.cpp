fc5d2ec9b53006052035033c377ebcdbbd8a3837
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-15 22:57:01 +0530

    Add files via upload

---
+++ Data types sample program.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a = 10;
+	float b = 5.5;
+	double c = 20.123;
+	char d ='x';
+	printf("Integer:%d\n",a);
+	printf("Float:%.2f\n",b);
+	printf("Double:%.3lf\n",c);
+	printf("Character:%c\n",d);
+	return 0;
+}
