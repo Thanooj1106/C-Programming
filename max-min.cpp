a2a354e1682353fb209089ef1b53b9f453fa0ad0
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-22 23:32:19 +0530

    Add files via upload

---
+++ max-min.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c,max,min;
+  printf("Enter three numbers:");
+  scanf("%d%d%d",&a,&b,&c);
+  max = min = a;
+  if (b>max)max=b;
+  if (c>max)max=c;
+  if (b<min)min=b;
+  if (c<min)min=c;
+  printf("max=%d\n",max);
+  printf("min=%d\n",min);
+  return 0;
+    
+}
