3329f3c84b16292006956f722e7554bbdf641819
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-23 21:24:18 +0530

    Add files via upload

--- Sum of digits of a number.cpp
+++ Sum of digits of a number.cpp
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int n,sum =0;
-	printf("Enter a number:");
+	int n,sum,r;
+	printf("Enter number:");
 	scanf("%d",&n);
-	while (n!=0)
+	sum =0;
+	while (n>0)
 	{
-		sum += n%10;
-		n =n/10;
+		r = n%10;
+		sum = sum +r;
+		n=n/10;
 	}
-	printf("Sum of digits = %d",sum);
+	printf("The sum =%d",sum);
 	return 0;
 }
