892ca8799d4202b47934bf906f6088c9f73aef59
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-01-28 15:56:14 +0530

    Add files via upload

---
+++ Hollow pattern.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<5;j++)
+		{
+		if((i==1&&j==2)||
+	    (i==2&&j==2)||
+	    (i==3&&j==2))
+	    printf(" ");
+	    else
+	    printf("*");
+	    }
+	    printf("\n");
+	}
+}
