0253815ae730dd8a63336316a8ace9f595618fe4
Thanooj1106 <thanoojkumarreddy@gmail.com>
2026-02-10 21:03:32 +0530

    Add files via upload

--- Prime number.cpp
+++ Prime number.cpp
@@ -2,19 +2,19 @@
 int main()
 {
 	int n,i,flag=1;
-	printf("Enter number:");
+	printf("Enter n:");
 	scanf("%d",&n);
-	for (i=2;i<n;i++)
+	for(i=2;i<n;i++)
 	{
 		if(n%i==0)
-		{
-			flag=0;
-			break;
-		}
-	}
-	if(flag==0)
-	printf("Not a prime number");
-	else
+	{
+		flag=0;
+		break;
+    }
+    }
+    if(!flag)
+	printf("Not a Prime number");
+	else 
 	printf("Prime number");
 	return 0;
 }
